e81be9a6da5fb0024161183bc9c8f5c0bf45ce6a
hado66 <632579497@qq.com>
2020-07-05 12:36:41 +0800

    完成导航

---
+++ src/components/comment/navbar/NavBar.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="nav-bar">
+      <div class="left"><slot name="left"></slot></div>
+      <div class="center"><slot name="center"></slot></div>
+      <div class="right"><slot name="right"></slot></div>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "NavBar"
+    }
+</script>
+
+<style scoped>
+  .nav-bar {
+    height: 49px;
+    line-height: 49px;
+    display: flex;
+  }
+  .left .right{
+    width: 49px;
+  }
+  /*.right {*/
+  /*  width: 49px;*/
+  /*}*/
+  .center {
+    background-color: red;
+    flex: 1;
+  }
+
+
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,30 @@
 <template>
-  <h2>首页</h2>
+  <div>
+    <nav-bar class="home-nav-bar">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <h2>首页</h2>
+
+
+  </div>
+
 </template>
 
 <script>
+  import NavBar from "../../components/comment/navbar/NavBar";
   export default {
-    name: "Home"
+    name: "Home",
+    components:{
+      NavBar,
+    }
   }
 </script>
 
 <style scoped>
+  .home-nav-bar {
+    text-align: center;
+    background-color: pink;
+    color: #f6f6f6;
+  }
 
 </style>
